8f1d500734210581445cc3a464fabcd59571501f
kolchiibrahim-sys <kolchiibrahim@icloud.com>
2026-01-02 12:28:24 +0400

    Published

---
+++ CofiestoMobileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CofiestoMobileApp
+//
+//  Created by Kolchı Ibrahım on 02.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
