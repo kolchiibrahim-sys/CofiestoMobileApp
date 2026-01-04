346ac632d24ed5a78b43a6c91fe2cc219951fd23
kolchiibrahim-sys <kolchiibrahim@icloud.com>
2026-01-04 12:51:52 +0400

    Extentions

--- CofiestoMobileApp/ViewControllers/MenuViewController.swift
+++ CofiestoMobileApp/ViewControllers/MenuViewController.swift
@@ -43,3 +43,36 @@ class MenuViewController: UIViewController,
         return cell
     }
 }
+extension MenuViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let padding: CGFloat = 16 * 3   // sol + sag + orta bowluq
+        let availableWidth = collectionView.frame.width - padding
+        let itemWidth = availableWidth / 2
+
+        return CGSize(width: itemWidth, height: 220)
+    }
+}
+extension MenuViewController {
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+}
