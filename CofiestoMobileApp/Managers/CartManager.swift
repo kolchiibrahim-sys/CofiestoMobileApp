59a6fd969827ada8f9d2cec7c6d28cf6805eb818
kolchiibrahim-sys <kolchiibrahim@icloud.com>
2026-01-04 14:13:42 +0400

    Add Quentify +1

--- CofiestoMobileApp/Managers/CartManager.swift
+++ CofiestoMobileApp/Managers/CartManager.swift
@@ -6,9 +6,26 @@
 //
 
 import Foundation
+
 class CartManager {
+
     static let shared = CartManager()
     private init() {}
 
-    var items: [CartItem] = []
+    private(set) var items: [CartItem] = []
+
+    func add(coffee: CoffeeItem) {
+        
+        if let index = items.firstIndex(where: { $0.coffee.name == coffee.name }) {
+            items[index].quantity += 1
+        } else {
+            
+            let newItem = CartItem(coffee: coffee, quantity: 1)
+            items.append(newItem)
+        }
+    }
+
+    func totalItemsCount() -> Int {
+        return items.reduce(0) { $0 + $1.quantity }
+    }
 }
