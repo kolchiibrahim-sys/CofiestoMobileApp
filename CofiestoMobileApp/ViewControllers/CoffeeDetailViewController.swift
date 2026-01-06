bffc0732d9f58dfdd1fdb9b3b3b647ba8c4da992
kolchiibrahim-sys <kolchiibrahim@icloud.com>
2026-01-06 20:14:16 +0400

    Coffee Detail Upload

--- CofiestoMobileApp/ViewControllers/CoffeeDetailViewController.swift
+++ CofiestoMobileApp/ViewControllers/CoffeeDetailViewController.swift
@@ -22,16 +22,24 @@ class CoffeeDetailViewController: UIViewController {
     }
 
     private func configureUI() {
-        guard let item = item else {
-            print("CoffeeDetailViewController: item is nil")
-            return
-        }
+        guard let item = item else { return }
 
         nameLabel.text = item.name
         priceLabel.text = "$\(String(format: "%.2f", item.price))"
         imageView.image = UIImage(named: item.imageName)
 
+        // Button style
         addToCartButton.layer.cornerRadius = 14
+        addToCartButton.layer.shadowColor = UIColor.black.cgColor
+        addToCartButton.layer.shadowOpacity = 0.15
+        addToCartButton.layer.shadowOffset = CGSize(width: 0, height: 4)
+        addToCartButton.layer.shadowRadius = 10
+        imageView.layer.cornerRadius = 24
+        imageView.layer.maskedCorners = [
+            .layerMinXMaxYCorner,
+            .layerMaxXMaxYCorner
+        ]
+        imageView.clipsToBounds = true
     }
 
     @IBAction func addToCartTapped(_ sender: UIButton) {
