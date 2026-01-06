4c73b67a9351c68d6a1fd013928dc98a58dcaec9
kolchiibrahim-sys <kolchiibrahim@icloud.com>
2026-01-07 03:18:26 +0400

    Tab Bar Controllers

---
+++ CofiestoMobileApp/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  CofiestoMobileApp
+//
+//  Created by Kolchı Ibrahım on 07.01.26.
+//
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
