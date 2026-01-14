4d1b9f02fa98154eeab07ea5ee51511117a410a9
kolchiibrahim-sys <kolchiibrahim@icloud.com>
2026-01-14 19:05:54 +0400

    Salma

--- CofiestoMobileApp/ViewController/CartCell.swift
+++ CofiestoMobileApp/ViewController/CartCell.swift
@@ -10,13 +10,11 @@ class CartCell: UITableViewCell {
 
     @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var ingredientsLabel: UILabel!
     @IBOutlet weak var quantityLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
 
     func configure(with cartItem: CartItem) {
         nameLabel.text = cartItem.item.name
-        ingredientsLabel.text = cartItem.item.ingredients.joined(separator: ", ")
         quantityLabel.text = "x\(cartItem.quantity)"
         priceLabel.text = String(format: "%.2f AZN", cartItem.totalPrice)
         productImageView.image = UIImage(named: cartItem.item.image) ?? UIImage(named: "placeholder")
